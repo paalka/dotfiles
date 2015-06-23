3dd514d68e33f006e5e485303ae2ca3618f5d9b3
Paul Karlsrud <paulkarlsrud@gmail.com>
2015-06-23 13:10:40 +0000

    Add a color reset to prevent the colors from leaking into ex. ls.

--- zsh/includes/prompt.zsh
+++ zsh/includes/prompt.zsh
@@ -74,7 +74,7 @@ function parse_git_state() {
 # If inside a Git repository, print its branch and state
 function git_prompt_string() {
   local git_where="$(parse_git_branch)"
-  [ -n "$git_where" ] && echo "on %{$fg[blue]%}${git_where#(refs/heads/|tags/)}$(parse_git_state)"
+  [ -n "$git_where" ] && echo "on %{$fg[blue]%}${git_where#(refs/heads/|tags/)}$(parse_git_state)%{$reset_color%}"
 }
 
 function current_pwd {
