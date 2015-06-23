4d45b350b598df63ceb52b3b38e279174d9cddb3
Paul Karlsrud <paulkarlsrud@gmail.com>
2015-06-23 12:49:00 +0200

    Reorganized the files to be included.

--- install.py
+++ install.py
@@ -11,36 +11,37 @@ def create_symlink(path_to_link, real_file):
         print "Creating " + real_file + " -> " + path_to_link
         os.symlink(real_file, path_to_link)
 
-CWD = os.getcwd()
-HOME_DIR =  os.path.expanduser('~')
+if __name__ == "__main__":
+    CWD = os.getcwd()
+    HOME_DIR =  os.path.expanduser('~')
 
-print "Initilizing submodules..."
-call(["git", "submodule", "update", "--init"])
+    print "Initilizing submodules..."
+    call(["git", "submodule", "update", "--init"])
 
-print "Updating submodules..."
-call(["git", "submodule", "foreach", "git", "pull"])
+    print "Updating submodules..."
+    call(["git", "submodule", "foreach", "git", "pull"])
 
-# Find out which subdirectories to ignore
-to_ignore = []
-try:
-    with open("ignore_symlink") as inputfile:
-        to_ignore = inputfile.read().split()
-except IOError:
-    pass
+    # Find out which subdirectories to ignore
+    to_ignore = []
+    try:
+        with open("ignore_symlink") as inputfile:
+            to_ignore = inputfile.read().split()
+    except IOError:
+        pass
 
-print "Creating symlinks..."
-for item in os.listdir(CWD):
-    item_path = os.path.join(CWD, item)
-    if not item.startswith(".") and os.path.isdir(item_path) and item not in to_ignore:
-        path_to_link = os.path.join(HOME_DIR, "." + item)
-        real_folder = os.path.join(CWD, item)
+    print "Creating symlinks..."
+    for item in os.listdir(CWD):
+        item_path = os.path.join(CWD, item)
+        if not item.startswith(".") and os.path.isdir(item_path) and item not in to_ignore:
+            path_to_link = os.path.join(HOME_DIR, "." + item)
+            real_folder = os.path.join(CWD, item)
 
-        create_symlink(path_to_link, real_folder)
+            create_symlink(path_to_link, real_folder)
 
-        for item2 in os.listdir(item_path):
-            curr_item = os.path.join(item, item2)
-            if not item2.startswith(".") and curr_item not in to_ignore:
-                path_to_link = os.path.join(HOME_DIR, "." + item2)
-                real_file = os.path.join(CWD, curr_item)
+            for item2 in os.listdir(item_path):
+                curr_item = os.path.join(item, item2)
+                if not item2.startswith(".") and curr_item not in to_ignore:
+                    path_to_link = os.path.join(HOME_DIR, "." + item2)
+                    real_file = os.path.join(CWD, curr_item)
 
-                create_symlink(path_to_link, real_file)
+                    create_symlink(path_to_link, real_file)
