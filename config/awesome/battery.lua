09e6388f3eac28d997c23c28504e819b6d08769d
Paul Karlsrud <paulkarlsrud@gmail.com>
2015-11-04 17:51:43 +0100

    Add some basic Awesome config files.

---
+++ config/awesome/battery.lua
@@ -0,0 +1,12 @@
+-- Create an ACPI widget
+batterywidget = widget({ type = "textbox" })
+batterywidget.text = " | Battery | "
+batterywidgettimer = timer({ timeout = 5 })
+batterywidgettimer:add_signal("timeout",
+  function()
+    fh = assert(io.popen("acpi | cut -d, -f 2,3 -", "r"))
+    batterywidget.text = " |" .. fh:read("*l") .. " | "
+    fh:close()
+  end
+)
+batterywidgettimer:start()
