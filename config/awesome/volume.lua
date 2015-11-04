381ef4645163cbbb17312b76f7ac503c748a5158
Paul Karlsrud <paulkarlsrud@gmail.com>
2015-11-04 20:12:09 +0100

    Use volume percentage rather than the weird square.

--- config/awesome/volume.lua
+++ config/awesome/volume.lua
@@ -6,24 +6,14 @@ function update_volume(widget)
     local status = fd:read("*all")
     fd:close()
 
-    local volume = tonumber(string.match(status, "(%d?%d?%d)%%")) / 100
-    -- volume = string.format("% 3d", volume)
+    local volume = tonumber(string.match(status, "(%d?%d?%d)%%"))
+    volume = string.format("%3d", volume)
 
     status = string.match(status, "%[(o[^%]]*)%]")
-
-    -- starting colour
-    local sr, sg, sb = 0x3F, 0x3F, 0x3F
-    -- ending colour
-    local er, eg, eb = 0xDC, 0xDC, 0xCC
-
-    local ir = volume * (er - sr) + sr
-    local ig = volume * (eg - sg) + sg
-    local ib = volume * (eb - sb) + sb
-    interpol_colour = string.format("%.2x%.2x%.2x", ir, ig, ib)
     if string.find(status, "on", 1, true) then
-        volume = " <span background='#" .. interpol_colour .. "'>    </span>"
+        volume = " <span>🔉" .. volume .. "%</span>"
     else
-        volume = " <span color='red' background='#" .. interpol_colour .. "'> M </span>"
+        volume = " <span color='red'>🔇 0%</span>"
     end
     widget.text = volume
  end
