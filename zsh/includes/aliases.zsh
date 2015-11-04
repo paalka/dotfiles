9ac5f47b4af3f553511aceeb787cab985571edab
Paul Karlsrud <paulkarlsrud@gmail.com>
2015-11-04 18:03:18 +0100

    Add a alias for a commonly used MOSH command.

--- zsh/includes/aliases.zsh
+++ zsh/includes/aliases.zsh
@@ -79,6 +79,7 @@ alias tmuxls="tmux list-sessions"
 # make rm command (potentially) less destructive
 alias 'rm=rm -i'
 alias 'py=python'
+alias 'mc=mosh cass'
 alias 'less=less -R'
 
 # Fetch the weather for Trondheim
