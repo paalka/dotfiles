d1ee86f9d0059ffbd0cba2c9dfafc82824354bbf
Paul Karlsrud <paulkarlsrud@gmail.com>
2015-11-05 19:20:16 +0100

    Make the battery widget adapt better to the different types of statuses.

--- config/awesome/battery.lua
+++ config/awesome/battery.lua
@@ -4,9 +4,11 @@ batterywidget.text = "  [Battery] "
 batterywidgettimer = timer({ timeout = 5 })
 batterywidgettimer:add_signal("timeout",
   function()
-    fh = assert(io.popen("acpi | awk 1 ORS=', ' | cut -d, -f 2,4,5", "r"))
-    batterywidget.text = "  [" .. fh:read("*l") .. " ]"
-    fh:close()
+    bat0 = assert(io.popen("acpi | grep 'Battery 0:' | cut -d, -f 2,3 | xargs", "r"))
+    bat1 = assert(io.popen("acpi | grep 'Battery 1:' | cut -d, -f 2,3 | xargs", "r"))
+    batterywidget.text = " [ " .. bat0:read("*l") .. " ]  [ " .. bat1:read("*l") .. " ]"
+    bat0:close()
+    bat1:close()
   end
 )
 batterywidgettimer:start()
