978aee6bbded10aa5b5ffcf59439af2d003b2e65
Paul Karlsrud <paulkarlsrud@gmail.com>
2015-11-10 16:12:19 +0100

    Display a message if both of the batteries have less then 10% power left.

--- config/awesome/battery.lua
+++ config/awesome/battery.lua
@@ -4,11 +4,34 @@ batterywidget.text = "  [Battery] "
 batterywidgettimer = timer({ timeout = 5 })
 batterywidgettimer:add_signal("timeout",
   function()
+    -- TODO: Rewrite this to get the total of all batteries, and put them in
+    -- the same indicator, to avoid cluttering the menubar.
     bat0 = assert(io.popen("acpi | grep 'Battery 0:' | cut -d, -f 2,3 | xargs", "r"))
     bat1 = assert(io.popen("acpi | grep 'Battery 1:' | cut -d, -f 2,3 | xargs", "r"))
+
     batterywidget.text = " [ " .. bat0:read("*l") .. " ]  [ " .. bat1:read("*l") .. " ]"
     bat0:close()
     bat1:close()
+
+    if batteryPercent("BAT0") < 10 and batteryPercent("BAT1") < 10 then
+        naughty.notify({ title  = "Low battery Warning"
+                                , text       = "Less than 10% battery left!"
+                                , timeout    = 5
+                                , position   = "top_right"
+                                , fg         = beautiful.fg_focus
+                                , bg         = beautiful.bg_focus
+                                })
+    end
   end
 )
+function batteryPercent(battery_name)
+    local adapter_current_file = io.open("/sys/class/power_supply/" .. battery_name .. "/energy_now")
+    local adapter_total_capacity_file = io.open("/sys/class/power_supply/" .. battery_name .. "/energy_full")
+    local adapter_current_capacity = adapter_current_file:read()
+    local adapter_total_capacity = adapter_total_capacity_file:read()
+
+    -- Return the percentage as an int.
+    return tonumber(math.floor(adapter_current_capacity * 100 / adapter_total_capacity))
+end
+
 batterywidgettimer:start()
