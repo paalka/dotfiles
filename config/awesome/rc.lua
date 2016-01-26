67460143762efebc2392b6b9f979587e7de7c5f1
Pål Karlsrud <paulkarlsrud@gmail.com>
2016-01-26 21:44:52 +0100

    Ensure that a urxvtd deamon is started if we attempt to open a urxvtc terminal.

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -50,7 +50,7 @@ end
 beautiful.init(awful.util.getdir("config") .. "/themes/default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "urxvtc"
+terminal = "urxvtcd"
 editor = os.getenv("EDITOR") or "editor"
 editor_cmd = terminal .. " -e " .. editor
 
