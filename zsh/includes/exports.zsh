f73d05da07b560752de3b219b0cab8e43b0aee3f
Pål Karlsrud <paulkarlsrud@gmail.com>
2016-07-19 20:23:46 +0200

    Make sure that all windows created by Java is handled correctly by Awesome.

--- zsh/includes/exports.zsh
+++ zsh/includes/exports.zsh
@@ -1,3 +1,6 @@
 export EDITOR=vim
 export TERMINAL=urxvt
 export SSH_ENV="$HOME/.ssh/environment"
+
+# Make sure that all windows created by java is picked up by the WM.
+export _JAVA_AWT_WM_NONREPARENTING=1
