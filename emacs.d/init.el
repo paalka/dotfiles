058e47cda8feee1ae96c0aadc23ec59d725f6b0d
Pål Karlsrud <paulkarlsrud@gmail.com>
2016-11-23 10:09:37 +0100

    Try to make the emacs mode line more useful by using a custom format.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -4,6 +4,55 @@
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 (setq inhibit-splash-screen t)
 
+(setq-default mode-line-format
+ '(;; Position, including warning for 80 columns
+   (:propertize "%4l:" face mode-line-position-face)
+   (:eval (propertize "%3c" 'face
+		      (if (>= (current-column) 80)
+			  'mode-line-80col-face
+			'mode-line-position-face)))
+   mode-line-client
+   "  "
+   ;; read-only or modified status
+   (:eval
+    (cond (buffer-read-only
+	   (propertize " RO " 'face 'mode-line-read-only-face))
+	  ((buffer-modified-p)
+	   (propertize " ** " 'face 'mode-line-modified-face))
+	  (t "      ")))
+   "    "
+   ;; directory and buffer/file name
+   (:propertize (:eval (shorten-directory default-directory 30))
+		face mode-line-folder-face)
+   (:propertize "%b"
+		face mode-line-filename-face)
+   " %n "
+   ;; mode indicators: vc, recursive edit, major mode, minor modes, process, global
+   (vc-mode vc-mode)
+   "  %["
+   (:propertize mode-name
+		face mode-line-mode-face)
+   "%] "
+   (:eval (propertize (format-mode-line minor-mode-alist)
+		      'face 'mode-line-minor-mode-face))
+   (:propertize mode-line-process
+		face mode-line-process-face)
+   (global-mode-string global-mode-string)
+   ))
+
+(defun shorten-directory (dir max-length)
+  "Show up to `max-length' characters of a directory name `dir'."
+  (let ((path (reverse (split-string (abbreviate-file-name dir) "/")))
+	(output ""))
+    (when (and path (equal "" (car path)))
+      (setq path (cdr path)))
+    (while (and path (< (length output) (- max-length 4)))
+      (setq output (concat (car path) "/" output))
+      (setq path (cdr path)))
+    (when path
+      (setq output (concat ".../" output)))
+        output))
+
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 (load-theme 'gruber-darker t)
 
