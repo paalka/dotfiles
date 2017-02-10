86fdc1ea50ed9221fbd01cc7869a97bce21c7187
Pål Karlsrud <paulkarlsrud@gmail.com>
2017-02-10 17:52:06 +0100

    Remove a bunch of tiling modes which I never use.

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -70,11 +70,8 @@ layouts =
 {
     awful.layout.suit.tile,
     awful.layout.suit.fair,
-    awful.layout.suit.fair.horizontal,
-    awful.layout.suit.tile.left,
     awful.layout.suit.floating,
     awful.layout.suit.tile.bottom,
-    awful.layout.suit.tile.top,
 }
 -- }}}
 
