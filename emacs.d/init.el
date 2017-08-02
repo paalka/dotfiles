fbb9f8ca9a9914e38f262c35557d1d3260169983
Pål Karlsrud <paulkarlsrud@gmail.com>
2017-08-02 14:29:24 +0200

    Ensure that the window title is always set when using emacs.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -214,3 +214,14 @@
 
 (editorconfig-mode 1)
 (show-paren-mode 1)
+
+(defun xterm-title-update ()
+  (interactive)
+  (send-string-to-terminal (concat "\033]1; " (buffer-name) "\007"))
+  (if buffer-file-name
+      (send-string-to-terminal (concat "\033]2; " (abbreviate-file-name (buffer-file-name)) " - emacs"  "\007"))
+    (send-string-to-terminal (concat  "\033]2; " (buffer-name) "\007"))))
+
+
+(if (not window-system)
+    (add-hook 'post-command-hook 'xterm-title-update))
