22fd68134e457078a65d72a620fa60726f7a8621
Pål Karlsrud <paulkarlsrud@gmail.com>
2017-10-09 15:53:35 +0200

    Add [emacs] to the frame title, as this makes the windows easier to find.

--- emacs.d/modules/term-title-update.el
+++ emacs.d/modules/term-title-update.el
@@ -3,7 +3,7 @@
   (interactive)
   (send-string-to-terminal (concat "\033]1;" (buffer-name) "\007"))
   (if buffer-file-name
-      (send-string-to-terminal (concat "\033]2;" (shorten-directory (buffer-file-name) 50) " - emacs"  "\007"))
+      (send-string-to-terminal (concat "\033]2;" "[emacs] " (shorten-directory (buffer-file-name) 50)  "\007"))
     (send-string-to-terminal (concat  "\033]2;" (buffer-name) "\007"))))
 
 
