c18b20a904aaf11716ee93a9be3fcc0a267d045c
Pål Karlsrud <paulkarlsrud@gmail.com>
2018-10-23 18:02:22 +0200

    Add some basic org capture templates.

--- emacs.d/modules/org-cfg.el
+++ emacs.d/modules/org-cfg.el
@@ -3,3 +3,19 @@
 
 (provide 'org-cfg)
 (add-hook 'org-mode-hook 'auto-fill-mode)
+
+(setq org-capture-templates
+      '(("t" "Todo" entry (file+headline "~/documents/org/todos/todos.org" "Tasks")
+	 "* TODO %?\n  %i\n")))
+
+(defun my/org-file-by-date ()
+  "Create an Org file with current time as name."
+  (find-file (format-time-string "~/documents/org/journal/%Y-%m-%d.org")))
+
+(add-to-list 'org-capture-templates
+	     '("j" "Journal" plain
+	       (function my/org-file-by-date)
+	       ""))
+
+
+(setq org-agenda-files (list "~/documents/org/todos/todos.org"))
