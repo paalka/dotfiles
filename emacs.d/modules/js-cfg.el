7009d6cae65e61d8cdf2c37d4d235479132d41da
Pål Karlsrud <paulkarlsrud@gmail.com>
2019-05-23 13:22:32 +0200

    Remove flow as I  don't use it anyways.

--- emacs.d/modules/js-cfg.el
+++ emacs.d/modules/js-cfg.el
@@ -6,11 +6,5 @@
  'js2-mode
  'lsp-ui
  'lsp-mode
- 'lsp-javascript-flow
 )
-(require 'lsp-javascript-flow)
-(add-hook 'js-mode-hook #'lsp-javascript-flow-enable)
-(add-hook 'js2-mode-hook #'lsp-javascript-flow-enable) ;; for js2-mode support
-(add-hook 'rjsx-mode #'lsp-javascript-flow-enable) ;; for rjsx-mode support
-
 (provide 'js-cfg)
