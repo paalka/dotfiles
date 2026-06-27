7a050c90599c73085e63a23ad42ccb1ddac95a1a
Pål Karlsrud <5358991+paalka@users.noreply.github.com>
2026-06-27 13:46:17 +0100

    Fix install

--- install.sh
+++ install.sh
@@ -12,14 +12,8 @@ create_dotfile_symlink() {
 	fi
 }
 
-create_dotfile_symlink "alacritty.yml" ".alacritty.yml"
-
 create_dotfile_symlink "vim/vimrc" ".vimrc"
 create_dotfile_symlink "vim" ".vim"
-
-create_dotfile_symlink "Xresources" ".Xresources"
-create_dotfile_symlink "Xsession" ".xsession"
-create_dotfile_symlink "Xmodmap" ".Xmodmap"
 
 create_dotfile_symlink "zsh/zshrc" ".zshrc"
 create_dotfile_symlink "zsh" ".zsh"
@@ -28,23 +22,13 @@ create_dotfile_symlink "bash/bashrc" ".bashrc"
 
 create_dotfile_symlink "git/gitconfig" ".gitconfig"
 
-create_dotfile_symlink "urxvt" ".urxvt"
-
 create_dotfile_symlink "tmux/tmux.conf" ".tmux.conf"
 
-create_dotfile_symlink "bin" ".bin"
 create_dotfile_symlink "bg" ".bg"
-
-create_dotfile_symlink "config/awesome" ".config/awesome"
-create_dotfile_symlink "config/systemd" ".config/systemd"
 
 create_dotfile_symlink "gdb/gdbinit" ".gdbinit"
 create_dotfile_symlink "gdb/gdbinit-gef.py" ".gdbinit-gef.py"
 
 create_dotfile_symlink "psql/psqlrc" ".psqlrc"
 
-create_dotfile_symlink "emacs.d" ".emacs.d"
-
 create_dotfile_symlink "top/toprc" ".toprc"
-
-create_dotfile_symlink "mutt" ".mutt"
