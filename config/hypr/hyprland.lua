5ef3c47079946c1869eecde93194180c186abb00
Pål Karlsrud <5358991+paalka@users.noreply.github.com>
2026-06-28 12:01:42 +0100

    Add workspace rules

--- config/hypr/hyprland.lua
+++ config/hypr/hyprland.lua
@@ -152,12 +152,16 @@ hl.animation({ leaf = "zoomFactor",    enabled = true,  speed = 7,    bezier = "
 -- uncomment all if you wish to use that.
 -- hl.workspace_rule({ workspace = "w[tv1]", gaps_out = 0, gaps_in = 0 })
 -- hl.workspace_rule({ workspace = "f[1]",   gaps_out = 0, gaps_in = 0 })
--- hl.window_rule({
---     name  = "no-gaps-wtv1",
---     match = { float = false, workspace = "w[tv1]" },
---     border_size = 0,
---     rounding    = 0,
--- })
+hl.window_rule({
+    name  = "firefox workspace",
+    match = { class = "firefox-esr" },
+    workspace = 1,
+})
+hl.window_rule({
+    name  = "slack workspace",
+    match = { initial_title = "Slack" },
+    workspace = 4,
+})
 -- hl.window_rule({
 --     name  = "no-gaps-f1",
 --     match = { float = false, workspace = "f[1]" },
