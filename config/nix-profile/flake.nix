9f43f6c81ef8274862b34b645484ada641c8eaf2
Pål Karlsrud <5358991+paalka@users.noreply.github.com>
2026-06-28 13:56:54 +0100

    Manage some packages with nix

---
+++ config/nix-profile/flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Personal package set";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      packages.${system}.default = pkgs.buildEnv {
+        name = "my-packages";
+        paths = with pkgs; [
+          neovim
+          ripgrep
+          fd
+          fzf
+        ];
+      };
+    };
+}
