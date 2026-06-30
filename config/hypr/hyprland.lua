6d752553a29d20a67b23bb9602ce0159b01e5783
Pål Karlsrud <5358991+paalka@users.noreply.github.com>
2026-06-30 10:28:10 +0200

    Use preferred resolution

--- config/hypr/hyprland.lua
+++ config/hypr/hyprland.lua
@@ -5,7 +5,7 @@
 -- See https://wiki.hypr.land/Configuring/Basics/Monitors/
 hl.monitor({
     output   = "",
-    mode     = "1920x1200@60",
+    mode     = "preferred",
     position = "auto",
     scale    = "1",
 })
